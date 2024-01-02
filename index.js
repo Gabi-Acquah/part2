680f042c60085b38220e1fa72b6e4f94b30a0d00
Gabi-Acquah <gabiacquah@gmail.com>
2024-01-02 16:22:38 +0000

    Update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.delete('/api/notes/:id', (req, res)=>{
   
   res.status(204).end()
 })
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port: ${PORT}`)
 })
